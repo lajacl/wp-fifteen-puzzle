d93f73b22fb65ee1e52941265bc0ac9c53948022
lajacl <lavette.clifton@gmail.com>
2025-07-24 23:41:54 -0400

    Set the correct image part for each tile

--- puzzle.js
+++ puzzle.js
@@ -1,14 +1,22 @@
-const numTiles = 15;
-const numRows = 4;
-const gridBoard = document.getElementById('grid-board');
-
 setupTiles();
 
 function setupTiles() {
+    const numTiles = 15;
+    const boardSize = 400;
+    const tileSize = 100;
+    const gridBoard = document.getElementById('grid-board');
+    let imgX = 0;
+    let imgY = 0;
+
     for (let i = 1; i <= numTiles; i++) {
         let div = document.createElement('div');
         div.className = 'tile';
         div.textContent = i;
+        div.style.backgroundPositionX = imgX + 'px';
+        div.style.backgroundPositionY = imgY + 'px';
+
+        imgX = (imgX > (-boardSize + tileSize)) ? (imgX - tileSize) : 0;
+        imgY = (imgX === 0) ? (imgY - tileSize) : imgY;
 
         gridBoard.appendChild(div);
     }
