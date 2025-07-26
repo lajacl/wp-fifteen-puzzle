6b8ab864a3456d89906c58f5e6f50b2122a613c4
lajacl <lavette.clifton@gmail.com>
2025-07-26 11:58:24 -0400

    Update display of grid items to use grid area

--- puzzle.js
+++ puzzle.js
@@ -1,44 +1,57 @@
-let emptySquare = { row: 3, col: 3 };
-
 setupGame();
 
 function setupGame() {
     const numTiles = 15;
     const boardSize = 400;
     const tileSize = 100;
-    const numRowsCols = 4;
     const gridBoard = document.getElementById('grid-board');
     let imgX = 0;
     let imgY = 0;
 
-    for (let i = 0; i < numTiles; i++) {
-        let div = document.createElement('div');
+    for (let i = 0; i <= numTiles; i++) {
+        const homeSquare = `square-${i + 1}`;
+        const div = document.createElement('div');
         div.className = 'tile';
-        div.textContent = i + 1;
-        div.dataset.row = Math.floor(i / numRowsCols);
-        div.dataset.col = i % numRowsCols;
-        div.style.backgroundPositionX = `${imgX}px`;
-        div.style.backgroundPositionY = `${imgY}px`;
+        div.textContent = (i != numTiles) ? i + 1 : '';
+        div.dataset.homeSquare = homeSquare;
+        div.style.gridArea = homeSquare;
 
-        imgX = (imgX > (-boardSize + tileSize)) ? (imgX - tileSize) : 0;
-        imgY = (imgX === 0) ? (imgY - tileSize) : imgY;
+        if (i != numTiles) {
+            div.style.backgroundImage = 'url(images/background.jpg)';
+            div.style.backgroundRepeat = 'no-repeat';
+            div.style.backgroundPositionX = `${imgX}px`;
+            div.style.backgroundPositionY = `${imgY}px`;
+
+            imgX = (imgX > (-boardSize + tileSize)) ? (imgX - tileSize) : 0;
+            imgY = (imgX === 0) ? (imgY - tileSize) : imgY;
+        } else {
+            div.id = "empty-square";
+        }
 
         gridBoard.appendChild(div);
     }
 
-    emptySquare = { row: 3, col: 3 };
     updateMoveablePieces();
 }
 
 function updateMoveablePieces() {
+    const numRowsCols = 4;
+    const emptyTile = document.getElementById('empty-square');
+    const emptyGridArea = emptyTile.style.gridArea;
+    const emptySquare = Number(emptyGridArea.replace('square-', ''));
     const tiles = document.getElementsByClassName('tile');
-    for (const tile of tiles) {
-        let tileRow = Number(tile.dataset.row);
-        let tileCol = Number(tile.dataset.col);
 
-        if ((tileRow == emptySquare.row && (tileCol + 1 == emptySquare.col || tileCol - 1 == emptySquare.col)) ||
-            (tileCol == emptySquare.col && (tileRow + 1 == emptySquare.row || tileRow - 1 == emptySquare.row))) {
+    for (const tile of tiles) {
+        if (tile.id == 'empty-square') return;
+
+        const tileGridArea = tile.style.gridArea;
+        const tileSquare = Number(tileGridArea.replace('square-', ''));
+
+        if ((tileSquare + numRowsCols == emptySquare) || (tileSquare - numRowsCols == emptySquare) ||
+            (emptySquare % 4 != 0 && tileSquare - 1 == emptySquare) || (emptySquare % 4 != 1 && tileSquare + 1 == emptySquare)) {
             tile.classList.add('moveablepiece');
+        } else {
+            tile.classList.remove('moveablepiece');
         }
     }
 }
